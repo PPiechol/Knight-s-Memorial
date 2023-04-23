1d90d32a5e68e2d17dde0eb4e672b3199e975e6b
Yoshi1722 <palmaolaf@gmail.com>
2023-04-23 17:31:41 +0200

    Statystyki postaci + wbijanie statystyk (demo)
    Wyświetlanie i aktualizowanie danych postaci //wczesna wersja

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
+
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 
 namespace WindowsFormsApp1
@@ -14,25 +17,12 @@ namespace WindowsFormsApp1
     public partial class MainForm : Form
     {
         Display Ekran_glowny;
-        PictureBox pictureBox1;
-
-
+        
         public MainForm()
         {
             InitializeComponent();
 
-            Panel panelLeft = new Panel();
-            panelLeft.Location = new Point(0, 0);
-            panelLeft.Size = new Size(Screen.PrimaryScreen.Bounds.Width / 5, 250);
-            panelLeft.Visible = true;
-            
-            this.Controls.Add(panelLeft);
-            pictureBox1 = new PictureBox();
-            Player player = new Player(pictureBox1, 1, 35, 8, 10, 1, panelLeft);
-
         }
-
-
 
 
         private void KeyIsDown(object sender, KeyEventArgs e)
@@ -42,9 +32,11 @@ namespace WindowsFormsApp1
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            
             Ekran_glowny = new Display(this.Width, this.Height, this);
             this.Controls.Add(Ekran_glowny);
             Ekran_glowny.Load_Level(1, 0, 0);
+
         }
     }
 }
